9150713303c8f384d550d53a34229635de97f044
nrr <nickrrau@gmail.com>
2024-05-11 14:08:23 -0400

    Match andrewrk/libz, remove uneeded files Lifted from Andrew's repo, nothing special done here

---
+++ build.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const lib = b.addStaticLibrary(.{
+        .name = "z",
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+    lib.linkLibC();
+    lib.addCSourceFiles(.{
+        .files = &.{
+            "adler32.c",
+            "crc32.c",
+            "deflate.c",
+            "infback.c",
+            "inffast.c",
+            "inflate.c",
+            "inftrees.c",
+            "trees.c",
+            "zutil.c",
+            "compress.c",
+            "uncompr.c",
+            "gzclose.c",
+            "gzlib.c",
+            "gzread.c",
+            "gzwrite.c",
+        },
+        .flags = &.{"-std=c89"},
+    });
+    lib.installHeader(.{ .path = "zconf.h" }, "zconf.h");
+    lib.installHeader(.{ .path = "zlib.h" }, "zlib.h");
+    b.installArtifact(lib);
+}
