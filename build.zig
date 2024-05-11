c453b28cb062b727a619684949b329f61433cc44
nrr <nickrrau@gmail.com>
2024-05-11 14:49:13 -0400

    Change library artifact name to match library name

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const lib = b.addStaticLibrary(.{
-        .name = "z",
+        .name = "zlib",
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
